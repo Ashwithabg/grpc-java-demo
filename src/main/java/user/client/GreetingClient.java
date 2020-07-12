26a2d6a79ae3a84d33c720d6716e783f5b3b9f78
Ashwitha <ashwithabg95@gmail.com>
2020-07-12 22:05:42 +0530

    Add client implementation for greet RPC.

--- src/main/java/user/client/GreetingClient.java
+++ src/main/java/user/client/GreetingClient.java
@@ -1,6 +1,9 @@
 package user.client;
 
-import com.proto.user_greeting.*;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -9,13 +12,24 @@ public class GreetingClient {
         System.out.println("gRPC client");
         ManagedChannel managedChannel = ManagedChannelBuilder
                 .forAddress("localhost", 50051)
+                .usePlaintext()
                 .build();
-        System.out.println("creating stub");
-        UserServiceGrpc.UserServiceBlockingStub syncClient = UserServiceGrpc.newBlockingStub(managedChannel);
 
-        //do something
+        System.out.println("creating greet stub");
+        GreetServiceGrpc.GreetServiceBlockingStub syncClient = GreetServiceGrpc.newBlockingStub(managedChannel);
 
-        System.out.println("shutting client");
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Ash")
+                .setLastName("B G")
+                .build();
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+        GreetResponse greetResponse = syncClient.greet(greetRequest);
+
+        System.out.println(greetResponse.getResult());
+
+        System.out.println("shutting down client");
         managedChannel.shutdown();
     }
 }
